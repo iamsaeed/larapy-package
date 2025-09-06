fce69c92bc277752b82aaf4d487d03918682f72f
Ahmad <asklko2004@gmail.com>
2025-09-06 04:00:24 +0000

    changes

--- http/__init__.py
+++ http/__init__.py
@@ -4,8 +4,12 @@ from .request import Request
 from .response import Response
 from .kernel import Kernel
 
+# Alias for backwards compatibility
+HttpKernel = Kernel
+
 __all__ = [
     "Request",
     "Response",
     "Kernel",
+    "HttpKernel",
 ]
